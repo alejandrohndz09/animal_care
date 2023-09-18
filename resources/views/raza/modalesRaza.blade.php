34f2026b01c629dcc8a1996bb654c56ddc8d5719
alejandrohndz09 <molinaflavio0@gmail.com>
2023-09-18 10:29:38 -0600

    delete raza

--- resources/views/raza/modalesRaza.blade.php
+++ resources/views/raza/modalesRaza.blade.php
@@ -3,23 +3,23 @@
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content text-center">
             <div class="modal-header">
-                <h5 style="margin-left: auto; margin-right: auto;">¿Desea dar de baja este registro?</h5>
+                <h5 style="margin-left: auto; margin-right: auto;">¿Desea eliminar este registro?</h5>
             </div>
             <div class="modal-body text-center">
                 <!-- Utiliza la clase text-center para centrar los elementos -->
-                <p><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/User-avatar.svg/2048px-User-avatar.svg.png"
+                <p><img src="{{asset('img/huella.png')}}"
                         alt="user" class="picture " id="imagenModal"
-                        style="width: 35%; height: auto; margin-left: auto; margin-right: auto;"> </p>
+                        style="width: 15%; height: auto; margin-left: auto; margin-right: auto;"> </p>
                 <p>Código: <span id="modalRecordCodigo"></span></p>
                 <p>Nombre: <span id="modalRecordNombre"></span></p>
                 <p>Especie: <span id="modalRecordEspecie"></span></p>
-                <p>Raza: <span id="modalRecordRaza"></span></p>
+                
             </div>
             <div class="modal-footer text-center" style="margin-left: auto; margin-right: auto;">
 
                 <button id="confirmar" type="submit" class="button button-pri" style="margin-right: 40px">
                     <i class="svg-icon fas fa-check"></i>
-                    <span class="lable">Dar de baja</span></button>
+                    <span class="lable">Eliminar</span></button>
                 <button type="button" class="button button-red" data-bs-dismiss="modal"> <i
                         class="svg-icon fas fa-xmark"></i>
                     <span class="lable">Cancelar</span> </button>
