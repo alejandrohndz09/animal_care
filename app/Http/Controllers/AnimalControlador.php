72790c39146a0ac53424221eacb04e219898df45
alejandrohndz09 <molinaflavio0@gmail.com>
2023-09-24 00:27:48 -0600

    a

--- app/Http/Controllers/AnimalControlador.php
+++ app/Http/Controllers/AnimalControlador.php
@@ -247,7 +247,7 @@ class AnimalControlador extends Controller
         $miembros->estado = '1';
         $miembros->save();
         return view('animal.index')->with([
-            'animales' => Animal::where('estado', 1)->get()
+            'animales' => Animal::where('estado',1)->get()
         ]);
     }
 }
